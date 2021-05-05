ecc6e012eb34c91c8ac1224f69cefc83b95386a9
mortuza19 <sahilmortuza.90@gmail.com>
2021-05-05 19:35:52 +0530

    changing project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/jest-demo'),
+      dir: require('path').join(__dirname, './coverage/Post-Details'),
       subdir: '.',
       reporters: [
         { type: 'html' },
